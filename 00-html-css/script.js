8ef22da2d2d3e5f00d5ec649c61e2a7f45fbb16e
Domenico Pagano <dpaganoh@gmail.com>
2025-10-08 13:08:13 -0500

    más js

--- 00-html-css/script.js
+++ 00-html-css/script.js
@@ -1,3 +1,4 @@
+/*
 const botones = document.querySelectorAll('.results__item-button');
 botones.forEach(boton => {
     boton.addEventListener('click', () => {
@@ -6,3 +7,19 @@ botones.forEach(boton => {
       boton.disabled = true;
     })
 });
+*/
+
+const jobsList = document.querySelector('#jobs-list');
+jobsList?.addEventListener('click', (event) => {
+    const element = event.target;
+    if(element.classList.contains('results__item-button')){
+        element.textContent = '¡Aplicado!';
+        element.classList.add('is-applied')
+        element.disabled = true;
+    }
+});
+
+const filter = document.querySelector('#filter-technology')
+filter.addEventListener('change', () => {
+    console.log(filter.value)
+})
