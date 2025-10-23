70abd112c529bc4a36ca840cce7d6321f274070a
Domenico Pagano <dpaganoh@gmail.com>
2025-10-23 09:18:16 -0500

    apply button func

--- 00-html-css/script.js
+++ 00-html-css/script.js
@@ -9,16 +9,22 @@ botones.forEach(boton => {
 });
 */
 
+// Apply button
 const jobsList = document.querySelector('#jobs-list');
-jobsList?.addEventListener('click', (event) => {
+const callToAction = document.querySelector('.call-to-action');
+function handleApplyClick(event){
     const element = event.target;
     if(element.classList.contains('apply-button')){
         element.textContent = '¡Aplicado!';
         element.classList.add('is-applied')
         element.disabled = true;
     }
-});
+}
+jobsList?.addEventListener('click', handleApplyClick);
+callToAction?.addEventListener('click', handleApplyClick);
 
+
+// Filter
 const filter = document.querySelector('#filter-technology')
 filter.addEventListener('change', () => {
     console.log(filter.value)
