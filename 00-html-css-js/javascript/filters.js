5c1f9645ccb0fd76c393a2bfff05164a6a47163e
Domenico Pagano <dpaganoh@gmail.com>
2025-10-25 09:43:09 -0500

    js finalizado

--- 00-html-css-js/javascript/filters.js
+++ 00-html-css-js/javascript/filters.js
@@ -2,7 +2,11 @@
 const filterByLocation = document.querySelector('#filter-location');
 const filterByLevel = document.querySelector('#filter-nivel');
 const filterByTech = document.querySelector('#filter-technology');
+const searchBar = document.querySelector('#empleos-search-input');
 
+
+
+// Filtro por location, level y technology:
 const handleFilter = (value, attribute) => {
     const selectedValue = value.trim().toLowerCase();
     const jobs = document.querySelectorAll('.results__item')
@@ -20,7 +24,21 @@ const handleFilter = (value, attribute) => {
         job.classList.toggle('is-hidden', !isShown)
     })   
 }
-
 filterByLocation.addEventListener('change', e => handleFilter(e.target.value, 'data-modalidad'))
 filterByLevel.addEventListener('change', e => handleFilter(e.target.value, 'data-nivel'))
 filterByTech.addEventListener('change', e => handleFilter(e.target.value, 'data-technology'))
+
+
+
+// Filtro por texto ingresado en el input:
+const handleSearch = (e) => {
+    const query = e.target.value.trim().toLowerCase();
+    const jobs = document.querySelectorAll('.results__item');
+
+    jobs.forEach(job => {
+        const title = job.querySelector('.results__item-title')?.textContent.toLowerCase() || '';
+        const matches = title.includes(query);
+        job.classList.toggle('is-hidden', !matches);
+    })
+}
+searchBar.addEventListener('input', e => handleSearch(e))
