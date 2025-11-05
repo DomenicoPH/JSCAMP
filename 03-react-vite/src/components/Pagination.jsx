ef20ed225f2108da4f397f0407e8ab6f0cf24d9c
Domenico Pagano <dpaganoh@gmail.com>
2025-11-04 22:09:22 -0500

    pagination

--- 03-react-vite/src/components/Pagination.jsx
+++ 03-react-vite/src/components/Pagination.jsx
@@ -1,17 +1,23 @@
 export function Pagination({ currentPage = 1, totalPages = 10 }){
 
     const pages = Array.from({length: totalPages}, (_, i) => i + 1);
+    const isFirstPage = currentPage === 1;
+    const isLastPage = currentPage === totalPages;
+
+    const stylePrevButton = isFirstPage ? { PointerEvent: 'none', opacity: 0.25 } : {}
+    const styleNextButton = isLastPage ? { PointerEvent: 'none', opacity: 0.25 } : {}
 
     return(
       <nav className="pagination">
-        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M15 18l-6-6 6-6"/></svg></a>
+
+        <a style={stylePrevButton} href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M15 18l-6-6 6-6"/></svg></a>
         {pages.map(page => (
             <a 
                 href="#"
                 className={currentPage === page ? 'is-active': ''}
             >{page}</a>
         ))}
-        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M9 6l6 6-6 6"/></svg></a>
+        <a style={styleNextButton} href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M9 6l6 6-6 6"/></svg></a>
       </nav>
     )
 };
