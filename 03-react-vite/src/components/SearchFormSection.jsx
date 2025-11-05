23f9942ef624d2303629a39160c3dddf323f2e87
Domenico Pagano <dpaganoh@gmail.com>
2025-11-05 16:53:52 -0500

    SearchFormSection

--- 03-react-vite/src/components/SearchFormSection.jsx
+++ 03-react-vite/src/components/SearchFormSection.jsx
@@ -1,6 +1,11 @@
+import { useId } from 'react';
 import styles from './SearchFormSection.module.css';
 
 export function SearchFormSection(){
+  const idText = useId();
+  const idTechnology = useId();
+  const idLocation = useId();
+  const idExperienceLevel = useId();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -24,7 +29,7 @@ export function SearchFormSection(){
               </svg>
               <input 
                 className={styles.formInput} 
-                name="search" 
+                name={idText}
                 id="empleos-search-input" 
                 required type="text" 
                 placeholder="Buscar trabajos, empresas o habilidades" 
@@ -33,7 +38,7 @@ export function SearchFormSection(){
             </div>
 
             <div className={styles.search__filters}>
-              <select id="filter-technology">
+              <select name={idTechnology} id="filter-technology">
                 <option value="">Tecnología</option>
                 <optgroup label="Tecnologías populares">
                   <option value="javascript">JavaScript</option>
@@ -51,7 +56,7 @@ export function SearchFormSection(){
                 <option value="php">PHP++</option>
               </select>
 
-              <select id="filter-location">
+              <select name={idLocation} id="filter-location">
                 <option value="">Ubicación</option>
                 <option value="remoto">Remoto</option>
                 <option value="cdmx">Ciudad de México</option>
@@ -60,7 +65,7 @@ export function SearchFormSection(){
                 <option value="barcelona">Barcelona</option>
               </select>
 
-              <select id="filter-nivel">
+              <select name={idExperienceLevel} id="filter-nivel">
                 <option value="">Nivel de experiencia</option>
                 <option value="junior">Junior</option>
                 <option value="mid">Mid-level</option>
