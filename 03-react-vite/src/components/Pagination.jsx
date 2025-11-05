ee5456d24c1256835744ce42682f2a765b241222
Domenico Pagano <dpaganoh@gmail.com>
2025-11-04 21:56:03 -0500

    pagination

--- 03-react-vite/src/components/Pagination.jsx
+++ 03-react-vite/src/components/Pagination.jsx
@@ -1,13 +1,17 @@
-export function Pagination(){
-  return(
-    <nav className="pagination">
-      <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M15 18l-6-6 6-6"/></svg></a>
-      <a href="#" className="is-active">1</a>
-      <a href="#">2</a>
-      <a href="#">3</a>
-      <a href="#">4</a>
-      <a href="#">5</a>
-      <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M9 6l6 6-6 6"/></svg></a>
-    </nav>
-  )
+export function Pagination({ currentPage = 1, totalPages = 10 }){
+
+    const pages = Array.from({length: totalPages}, (_, i) => i + 1);
+
+    return(
+      <nav className="pagination">
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M15 18l-6-6 6-6"/></svg></a>
+        {pages.map(page => (
+            <a 
+                href="#"
+                className={currentPage === page ? 'is-active': ''}
+            >{page}</a>
+        ))}
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M9 6l6 6-6 6"/></svg></a>
+      </nav>
+    )
 };
